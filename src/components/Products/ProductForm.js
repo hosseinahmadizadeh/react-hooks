6e5547845093cc8776a2a4b492a6f6c882f2e871
hosseinahmadizadeh <hosseinahmadizadeh1377@gmail.com>
2023-06-21 14:38:16 +0330

    first commit

---
+++ src/components/Products/ProductForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+
+import "./ProductForm.css";
+
+const ProductForm = React.memo((props) => {
+  // const inputState = useState({ title: "", amount: "" });
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <section className="product-form">
+      <Card>
+        <form onSubmit={submitHandler}>
+          <div className="form-control">
+            <label htmlFor="title">عنوان</label>
+            <input
+              type="text"
+              id="title"
+              // value={inputState[0].title}
+              // onChange={(event) => inputState[1]({ title: event.target.value })}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="amount">تعداد</label>
+            <input
+              type="number"
+              id="amount"
+              // value={inputState[0].amount}
+              // onChange={(event) =>
+              //   inputState[1]({ amount: event.target.value })
+              // }
+            />
+          </div>
+          <div className="product-form__actions">
+            <button type="submit">افزودن</button>
+          </div>
+        </form>
+      </Card>
+    </section>
+  );
+});
+
+export default ProductForm;
