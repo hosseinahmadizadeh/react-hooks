5c5b5b892fa060fcbf8dc31ec121cd96a72fa4b6
hosseinahmadizadeh <hosseinahmadizadeh1377@gmail.com>
2023-07-09 20:12:05 +0330

    sending http requests

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -8,14 +8,25 @@ const Products = () => {
   const [products, setProducts] = useState([]);
 
   const addProductHandler = (item) => {
-    setProducts((prevState) => {
-      return [
-        ...prevState,
-        {
-          id: Math.random().toString(),
-          ...item,
-        },
-      ];
+    fetch(
+      "https://react-hook-a1f92-default-rtdb.firebaseio.com/products.json",
+      {
+        method: "POST",
+        body: JSON.stringify(item),
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then((response) => {
+      response.json().then((responseData) => {
+        setProducts((prevState) => {
+          return [
+            ...prevState,
+            {
+              id: responseData.name,
+              ...item,
+            },
+          ];
+        });
+      });
     });
   };
   return (
