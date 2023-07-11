d565c728b8cd931307e8b7b93fd5b2b727df8aa2
hosseinahmadizadeh <hosseinahmadizadeh1377@gmail.com>
2023-07-11 18:29:29 +0330

    useReducer

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,33 +1,28 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useReducer } from "react";
 
 import ProductForm from "./ProductForm";
 import ProductList from "./ProductList";
 import Search from "./Search";
 
+const productReducer = (state, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.products;
+    case "ADD":
+      return [...state, action.product];
+    default:
+      throw new Error("Error");
+  }
+};
+
 const Products = () => {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    fetch("https://react-hook-a1f92-default-rtdb.firebaseio.com/products.json")
-      .then((response) => {
-        return response.json();
-      })
-      .then((responseData) => {
-        const loadedProducts = [];
-
-        for (const item in responseData) {
-          loadedProducts.push({
-            id: item,
-            title: responseData[item].title,
-            amount: responseData[item].amount,
-          });
-        }
-        setProducts(loadedProducts);
-      });
-  }, []);
+  const [products, dispatch] = useReducer(productReducer, []);
 
   const searchProductsHandler = useCallback((items) => {
-    setProducts(items);
+    // setProducts(items);
+    dispatch({ type: "SET", products: items });
   }, []);
 
   const addProductHandler = (item) => {
@@ -40,14 +35,19 @@ const Products = () => {
       }
     ).then((response) => {
       response.json().then((responseData) => {
-        setProducts((prevState) => {
-          return [
-            ...prevState,
-            {
-              id: responseData.name,
-              ...item,
-            },
-          ];
+        // setProducts((prevState) => {
+        //   return [
+        //     ...prevState,
+        //     {
+        //       id: responseData.name,
+        //       ...item,
+        //     },
+        //   ];
+        // });
+
+        dispatch({
+          type: "ADD",
+          product: { id: responseData.name, ...item },
         });
       });
     });
